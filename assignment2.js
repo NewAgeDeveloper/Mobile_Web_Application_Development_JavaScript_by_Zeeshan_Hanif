6c838c3809832148bea5cefda63da581797a539d
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2022-11-15 23:10:45 +0500

    Mobile Web Application Development by Sir Zeeshan
    Mobile Web Application Development by Sir Zeeshan Hanif Assignment 2 Solutions

---
+++ assignment2.js
@@ -0,0 +1,110 @@
+/* Question 1
+var city= prompt("Enter the name of city","Karachi");
+if(city=="Karachi" || city=="karachi"){
+    console.log("Welcome to the city of lights")
+} else {
+    console.log("Enter name again")
+}
+*/
+/* Question 2
+var signalColor=prompt("Enter the color of traffic light signal");
+if(signalColor=="Red" || signalColor=="red"){
+    console.log("Must Stop");
+} else if(signalColor=="Yellow" || signalColor=="yellow"){
+    console.log("Ready to Move");
+} else if(signalColor=="Green" || signalColor=="green"){
+    console.log("Move now");
+} else {
+    console.log("Enter value again")
+}
+*/
+
+/*
+//a
+var a = 4;
+if (++a === 5) {
+alert("given condition for variable a is true");
+}
+//b
+var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");
+}
+//c
+var c = 12;
+if (c++ === 13){
+alert("condition 1 is true");
+}
+if (c === 13){
+alert("condition 2 is true");
+}
+if (++c < 14){
+alert("condition 3 is true");
+}
+if(c === 14){
+alert("condition 4 is true");
+}
+//d
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");
+}
+//e
+if (true){
+    alert("True");
+}
+if (false){
+    alert("False");
+}
+//f
+if("car" < "cat"){
+    alert("car is smaller than cat");
+}
+*/
+
+
+/* Question 4
+var eng=parseInt(prompt("Enter marks in English"));
+var math=parseInt(prompt("Enter marks in Math"));
+var sci=parseInt(prompt("Enter marks in Science"));
+var sum=eng+math+sci;
+var percentage=sum/3;
+
+if(percentage>=80){
+    document.write("<b>Mark Sheet</b> <br><br>Total Marks : 300 <br> Marks obtained : "+sum+"<br> Percentage :"+percentage+"<br> Grade : A-one <br> Remarks : Excellent");
+}else if(percentage>=70){
+    document.write("<b>Mark Sheet</b> <br><br>Total Marks : 300 <br> Marks obtained : "+sum+"<br> Percentage :"+percentage+"<br> Grade : A <br> Remarks : Good");
+}else if(percentage>=60){
+    document.write("<b>Mark Sheet</b> <br><br>Total Marks : 300 <br> Marks obtained : "+sum+"<br> Percentage :"+percentage+"<br> Grade : B <br> Remarks : You need to improve");
+}else if(percentage<60){
+    document.write("<b>Mark Sheet</b> <br><br>Total Marks : 300 <br> Marks obtained : "+sum+"<br> Percentage :"+percentage+"<br> Grade : Fail <br> Remarks : Sorry");
+}
+*/
+
+
+/* Question 5
+var secNum=6;
+var num=parseInt(prompt("Enter any number"));
+if(num===secNum){
+    console.log("Bingo! Correct answer");
+}else if(num+1===secNum){
+    console.log("Close enough to the correct answer");
+}else{
+    console.log("Enter number again");
+}
+*/
+
+
+/* Question 6
+var num=parseInt(prompt("Enter any number"));
+if(num%2===0){
+    console.log("Entered number is Even number");
+}else{
+    console.log("Entered number is Odd number");
+}
+*/
+
+
+/* Question 7*/
